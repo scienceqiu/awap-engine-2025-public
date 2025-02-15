d2cf2dc430050a58f687c98c1e23ccca3d5b4a3e
Zanzao Chen <zanzaoc@andrew.cmu.edu>
2025-02-15 18:43:31 -0500

    healers should target own units

--- src/robot_controller.py
+++ src/robot_controller.py
@@ -1082,7 +1082,7 @@ class RobotController:
         
 
 
-    def explore_for_attack(self, explorer_unit_id: int, explore_building_id: int, target_unit_id: int):
+    def explore_for_attack(self, t_unit_id: int, explore_building_id: int, target_unit_id: int):
         '''
         Increases target unit strength by 2 (including those that originally has damage of 0)
         
@@ -1216,9 +1216,10 @@ class RobotController:
             print("can_heal_unit(): invalid attacking_unit_id")
             return False
         
-        if target_unit_id not in self.__game_state.units[self.get_enemy_team()]:
+        if target_unit_id not in self.__game_state.units[self.get_ally_team()]:
             print("can_heal_unit(): invalid target_unit_id")
             return False
+
 
 
         healer_unit = self.__game_state.get_unit_from_id(healer_id)
@@ -1260,7 +1261,7 @@ class RobotController:
             print("can_heal_unit(): invalid attacking_unit_id")
             return False
         
-        if target_unit_id not in self.__game_state.units[self.get_enemy_team()]:
+        if target_unit_id not in self.__game_state.units[self.get_ally_team()]:
             print("can_heal_unit(): invalid target_unit_id")
             return False
 
